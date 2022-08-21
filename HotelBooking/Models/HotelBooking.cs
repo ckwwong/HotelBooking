037374a22d918b38f244519706fb2e58ce3face5
Chris Wong <chrisw03@hotmail.co.uk>
2022-08-21 18:35:31 +0100

    Added Hotel Booking Model for Id, GuestName, DateStart, DateEnd

---
+++ HotelBooking/Models/HotelBooking.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; } 
+
+        public string GuestName { get; set; }
+
+        public DateTime CreatedDate { get; set; }   
+
+        public DateTime DateEnd { get; set; }
+        
+    }
+}
